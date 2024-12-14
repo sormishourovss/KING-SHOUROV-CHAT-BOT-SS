a32e67499e0511c24bc89c6db65f7bd1f891ada4
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-14 13:45:47 +0300

    Update npx2.js

--- scripts/commands/npx2.js
+++ scripts/commands/npx2.js
@@ -19,12 +19,12 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/Yc2atQe.mp4',
+      'https://i.imgur.com/4bpYajA.mp4',
       { responseType: 'stream' }
     )
   ).data;
 
-	if (body.indexOf("Love")==0 || body.indexOf("❤️‍🔥")==0 || body.indexOf("💌")==0 || body.indexOf("💘")==0 || body.indexOf("💟")==0 || body.indexOf("I love u")==0 || body.indexOf("I love you")==0 || body.indexOf("valobashi")==0 || body.indexOf("Valobashi")==0 || body.indexOf("🖤")==0) {
+	if (body.indexOf(";")==0 || body.indexOf(";")==0 || body.indexOf(";")==0 || body.indexOf(";")==0 || body.indexOf(";")==0 || body.indexOf("I Love U")==0 || body.indexOf("I love you")==0 || body.indexOf("valobashi")==0 || body.indexOf("Valobashi")==0 || body.indexOf("I LOVE U")==0) {
 		var msg = {
 				body: "ভালোবাসা সুন্দর🖤𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯",
 				attachment: media

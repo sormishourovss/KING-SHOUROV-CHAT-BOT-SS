5f77d8819275a67890dc126a16b903e87193a0a9
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-17 15:49:07 +0300

    Create 1111shourov.js

---
+++ scripts/commands/1111shourov.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "fahim.18",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf(";")==0 || (event.body.indexOf(";")==0 || (event.body.indexOf(",")==0 || (event.body.indexOf(";")==0)))) {
+		var msg = {
+				body: "ওরে কেউ মেনসন দিবি না ও বস সৌরভ এর ভালোবাসা and বউ 😍🥰 :))"
+    }
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
